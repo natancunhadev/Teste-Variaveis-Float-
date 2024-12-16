97e22e58c7358b6e5a8ebe0366df6d72b51f9752
Natan Cunha <104222003+natancunhadev@users.noreply.github.com>
2024-12-16 11:30:21 -0800

    Add files via upload

---
+++ teste variaveis float .py
@@ -0,0 +1,13 @@
+
+#flutuante: float 
+#python
+
+peso = 82.45
+salario = 399.99
+preco_bobs = 19.99
+calibre_corda = 10
+
+print(peso, type(peso))
+print(salario, type(salario))
+print(preco_bobs, type(preco_bobs))
+print(calibre_corda, type(calibre_corda))
